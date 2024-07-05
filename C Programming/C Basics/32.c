dc01572f67723e5852c0826e8367563b2bc059cd
2000pawan <yaduvanshi2000pawan@gmail.com>
2024-07-05 23:22:44 +0530

    Commit

---
+++ C Programming/C Basics/32.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,i;
+    printf("Enter the number: ");
+    scanf("%d",&a);
+    for (i==1;i<=100;i++){
+        if (i%a==3){
+            printf("%d\n",i);
+            }
+    }
+    return 0;
+}
