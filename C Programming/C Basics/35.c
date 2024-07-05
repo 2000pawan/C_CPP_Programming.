732fac7792c38947d198dd2650ca7037210e7964
2000pawan <yaduvanshi2000pawan@gmail.com>
2024-07-06 00:04:41 +0530

    Commit

--- C Programming/C Basics/35.c
+++ C Programming/C Basics/35.c
@@ -1,4 +1,15 @@
 #include<stdio.h>
 int main(){
+    int a,b;
+    printf("Enter first number:- ");
+    scanf("%d",&a);
+    printf("Enter second number:- ");
+    scanf("%d",&b);
+    if (a>b){
+        printf("The Pair is descending order!");
+    }
+    else{
+        printf("The Pair is ascending order!");
+        }
     return 0;
 }
