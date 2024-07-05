de3ab6eea3a23437904763be909f15884c1a0965
2000pawan <yaduvanshi2000pawan@gmail.com>
2024-07-05 17:05:13 +0530

    Commit

---
+++ C Programming/C Basics/26.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i;
+    printf("Even number between 1 to 50 (inclusive):= \n");
+    for (i=1;i<=50;i++){
+        if (i%2==0){
+            printf("%d ",i);
+            }
+    }
+    return 0;
+}
