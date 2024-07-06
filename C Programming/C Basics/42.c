7eb2902b8f06e50e4a122fdeee81159c049624d9
2000pawan <yaduvanshi2000pawan@gmail.com>
2024-07-06 15:18:58 +0530

    Commit

---
+++ C Programming/C Basics/42.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,i;
+    printf("Enter the number of lines:- ");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++){
+        printf("%d %d %d\n",i,i*i,i*i*i);
+    }
+    return 0;
+}
