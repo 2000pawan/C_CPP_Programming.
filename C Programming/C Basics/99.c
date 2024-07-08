0e5e73a6000918518d99cba23704b8d7f02f5287
2000pawan <yaduvanshi2000pawan@gmail.com>
2024-07-09 00:58:33 +0530

    Commit

--- C Programming/C Basics/99.c
+++ C Programming/C Basics/99.c
@@ -1,1 +1,39 @@
-hjk
+#include <stdio.h>
+void print_Histogram ( int *hist, int n );
+int main() {
+   int i, j;
+   int inputValue, hist_value=0;
+   printf("Input number of histogram bar (Maximum 10): \n");  
+   scanf("%d", &inputValue);
+   int hist[inputValue];
+   if (inputValue<=10)
+   {    
+    printf("Input the values between 0 and 10 (separated by space): \n");
+    for (i = 0; i < inputValue; ++i) {
+      scanf("%d", &hist_value);     
+      if (hist_value>=1 && hist_value<=10)
+	   hist[i] = hist_value;
+	   hist_value=0;
+     }
+    int results[10] = {0};
+    for(j = 0; j < inputValue; j++) {
+         if ( hist[j] == i){
+            results[i]++;
+         }
+    }
+    printf("\n");
+    print_Histogram(hist, inputValue);
+   }
+    return 0;
+}
+void print_Histogram(int *hist, int n) {
+      printf("\nHistogram:\n");
+	  int i, j;
+      for (i = 0; i < n; i++) {
+      for ( j = 0; j < hist[i]; ++j) {
+      printf("#");
+      }
+      printf("\n");
+   }
+}
+
